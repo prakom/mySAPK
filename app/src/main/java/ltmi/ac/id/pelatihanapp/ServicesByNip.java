7c27bbaa7b85279ba650e6ca6aaad968af24b097
hijaudaus <hijaudaus@gmail.com>
2017-11-23 14:23:34 +0700

    penambahan terima data id dan nama

--- app/src/main/java/ltmi/ac/id/pelatihanapp/ServicesByNip.java
+++ app/src/main/java/ltmi/ac/id/pelatihanapp/ServicesByNip.java
@@ -2,6 +2,7 @@ package ltmi.ac.id.pelatihanapp;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -18,9 +19,11 @@ import org.json.JSONTokener;
 public class ServicesByNip extends AsyncTask<Object, Integer, String> {
     String editNip;
     Context context;
-    public ServicesByNip(String editNip, Context context) {
+    MainActivity mainActivity;
+    public ServicesByNip(String editNip, Context context, MainActivity mainActivity) {
         this.editNip = editNip;
         this.context = context;
+        this.mainActivity=mainActivity;
     }
     @Override
     protected String doInBackground(Object... params) {
@@ -36,9 +39,14 @@ public class ServicesByNip extends AsyncTask<Object, Integer, String> {
             JSONTokener tokener = new JSONTokener(result);
             JSONObject jobj= (JSONObject)tokener.nextValue();
             JSONObject data=jobj.getJSONObject("data");
-            JSONObject instansi=data.getJSONObject("instansi");
-            String nama = instansi.getString("nama");
-            Toast.makeText(this.context, nama, Toast.LENGTH_LONG).show();
+            //JSONObject instansi=data.getJSONObject("instansi");
+           // String nama = instansi.getString("nama");
+            //Toast.makeText(this.context, nama, Toast.LENGTH_LONG).show();
+            String nip=data.getString("id");
+            String nama=data.getString("nama");
+
+            mainActivity.tampilData(nip,nama);
+
             Log.d("jsom_key",jobj.toString());
         } catch (JSONException e) {
             e.printStackTrace();
